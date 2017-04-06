9d75511704f2d0b78d5ec2d4c3b14d244398c8b9
zhaoxinfu <xinfu.zhao@tcl.com>
2017-04-06 13:39:26 +0800

    start

---
+++ app/src/main/java/com/study/b8a3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.study.b8a3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+}
