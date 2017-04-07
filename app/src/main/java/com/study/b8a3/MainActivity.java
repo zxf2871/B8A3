6a02a126dca64bca4c5c506d861fac412fd53aa4
zhaoxinfu <xinfu.zhao@tcl.com>
2017-04-07 10:03:04 +0800

    add menu

--- app/src/main/java/com/study/b8a3/MainActivity.java
+++ app/src/main/java/com/study/b8a3/MainActivity.java
@@ -2,7 +2,10 @@ package com.study.b8a3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.app_bar_search:
+                Toast.makeText(this, item.getTitle(),Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.app_bar_switch:
+                Toast.makeText(this, item.getTitle(),Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
