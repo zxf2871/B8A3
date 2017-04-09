666ee7e23c07760b0c58bf29197366d81386b18e
zxf2871 <zxf2871@163.com>
2017-04-09 13:32:07 +0800

    参数传递

--- app/src/main/java/com/study/b8a3/activity/SecondActivity.java
+++ app/src/main/java/com/study/b8a3/activity/SecondActivity.java
@@ -1,8 +1,11 @@
 package com.study.b8a3.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.study.b8a3.R;
 
@@ -11,9 +14,55 @@ import com.study.b8a3.R;
  */
 
 public class SecondActivity extends AppCompatActivity {
+    public static String TAG = SecondActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+        Toast.makeText(this, TAG + "onCreate", Toast.LENGTH_SHORT).show();
+        findViewById(R.id.btn_second_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, TAG + "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, TAG + "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, TAG + "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, TAG + "onPause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, TAG + "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, TAG + "onRestart", Toast.LENGTH_SHORT).show();
     }
 }
