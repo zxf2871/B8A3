64bd3d1aff99598031d7e168e2b1a311cb50882d
zhaoxinfu <xinfu.zhao@tcl.com>
2017-06-08 19:57:46 +0800

    学习gradle

---
+++ app/src/main/java/com/study/b8a3/activity/FirstFragment.java
@@ -0,0 +1,37 @@
+package com.study.b8a3.activity;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public class FirstFragment extends Fragment {
+    public static final String TAG = FirstFragment.class.getSimpleName();
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+    }
+}
