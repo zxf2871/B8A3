61902109299daab0ed77beccb4e6d5f56b7c7b47
zxf2871 <zxf2871@163.com>
2017-11-12 15:06:48 +0800

    去掉weex

--- app/src/main/java/com/study/b8a3/login/SplashActivity.java
+++ app/src/main/java/com/study/b8a3/login/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.study.b8a3.R;
 import com.study.b8a3.StubViewActivity;
 import com.study.b8a3.activity.HomeActivity;
+import com.study.b8a3.activity.MainActivity;
 import com.study.b8a3.main.BaseActivity;
 import com.study.b8a3.utils.Injection;
 
@@ -28,7 +29,7 @@ public class SplashActivity extends BaseActivity implements LoginContract.View{
 
     @Override
     public void loginSuccess() {
-        HomeActivity.startActivity(this, StubViewActivity.class);
+        HomeActivity.startActivity(this, HomeActivity.class);
         finish();
     }
 
